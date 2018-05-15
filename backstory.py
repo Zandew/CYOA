fb3ed0dad8d9375a12553b67381c1dba25c5170f
Zandew <39287272+Zandew@users.noreply.github.com>
2018-05-15 16:55:34 -0400

    Update and rename gameinfo.py to backstory.py

--- backstory.py
+++ backstory.py
@@ -1,4 +1,4 @@
-gameinfo = ['Welcome to the game!', 'The objective of this game is to defeat\
+gameinfo1 = ['Welcome to the game!', 'The objective of this game is to defeat\
 the boss and escape the locked room.', 'You have to escape by battling pokemon.\
 If you beat all the guards, you can face the boss!', 'Each pokemon has health and attack stats. The attack stat is split up\
 between minimum and maximum attack stats. The minimum attack \
@@ -8,5 +8,15 @@ Grass is super effective on water, water is super effective on fire and fire is 
 super effective on grass! There are also pokemon with no type that are only super \
 effective against other pokemon with no type. Type super effectiveness \
 increases damage!']
+
+gameinfo2 = ['You have waken up in a room with no apparent exit other than two doors. \
+You have no idea what has happened, \
+only that you were on your way to the prestigeous Pokemon Academy for \
+your first day of middle school with your very first pokemon. \
+Now, you appear to be in a locked room and only have that pokemon with you to help you escape. While \
+thinking about this situation, you can not help but wonder; why did this happen \
+to me? Where do these doors lead? How could I have ever ended up in this \
+situation...']
+
 
 
