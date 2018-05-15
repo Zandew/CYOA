fe7bcfca26841adb40195b12a1d3086f2636b6c8
Zandew <39287272+Zandew@users.noreply.github.com>
2018-05-15 16:52:18 -0400

    Add files via upload

---
+++ gameinfo.py
@@ -0,0 +1,12 @@
+gameinfo = ['Welcome to the game!', 'The objective of this game is to defeat\
+the boss and escape the locked room.', 'You have to escape by battling pokemon.\
+If you beat all the guards, you can face the boss!', 'Each pokemon has health and attack stats. The attack stat is split up\
+between minimum and maximum attack stats. The minimum attack \
+stat is 0.7 times the maximum attack stat. When you level up, you get 5 stat points to \
+allocate between health and attack.', 'Each pokemon is of a different type. There are grass,fire and water kinds.\
+Grass is super effective on water, water is super effective on fire and fire is \
+super effective on grass! There are also pokemon with no type that are only super \
+effective against other pokemon with no type. Type super effectiveness \
+increases damage!']
+
+
