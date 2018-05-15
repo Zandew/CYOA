a54e3e0cd2ca179a6003672ef436889cfa198bd9
Zandew <39287272+Zandew@users.noreply.github.com>
2018-05-15 17:15:12 -0400

    Update ICSSummative.py

--- ICSSummative.py
+++ ICSSummative.py
@@ -8,91 +8,7 @@ Python Version: 3.6
 import time
 import random
 import sys
-def room2():#Where you can level up your pokemon
-    print('Here you can level up your pokemon if you win a battle.')
-    wildencounter()
-    print (‘Looks like this room is a dead end!’)
-    print('You arrived back at the starting room!')
-    startingroom()
-def room1():#Where you can level up your pokemon, then you go into the guard room.
-    print('Here you can level up your pokemon if you win a battle.')
-    wildencounter()
-    print (‘You come across a huge hole and the only ways to get across are to walk over an old\
-bridge or to walk around the hole’)
-    print (‘Do you take the bridge or walk around the hole?’)
-    command = input().lower()
-    If ‘bridge’ in command:
-         print (‘While you are taking the bridge, the bridge collapses,  you fall down the hole and find a wild pokemon!!’)
-         wildencounter()
-         print (‘Luckily there is a ladder going up the hole, you take the ladder and reach the other side’)
-         
-    else:
-         print (‘You safely crossed the hole’)
-    pokecenter()
-    guardroom()
-def startingroom(): #This is the first room players end up in
-    command=input('Both rooms look like they contain wild pokemon. Turn left or right?: ')
-    if 'left' in command:
-        room1()
-    elif 'right' in command:
-        room2()
-    else:
-        print('Please enter a valid direction!')
-        startingroom()
-def pokecenter():
-    command = input('It looks like there is a Pokemon Center nearby, do you want to heal your pokemon?').lower()
-    if 'yes' in command:
-    	print ('Hello, and welcome to the Pokémon Center. We will heal your pokemon to full health.')
-    	starterpokemon.heal()
-    	print ('Please come again!')
-def guardroom():
-    print('This is the guard room! You have to face 3 pokemon back to back!')
-    guardbattle()
-    pokecenter()
-    bossbattle()
-print('Welcome to the game!')
-print('The objective of this game is to defeat the boss and escape the locked room.')
-print('You have to escape by battling pokemon. \
-If you beat all the guards, you can face the boss!')
-time.sleep(4)
-print('Each pokemon has health and attack stats. The attack stat is split up\
-between minimum and maximum attack stats. The minimum attack \
-stat is 0.7 times the maximum attack stat. When you level up, you get 5 stat points to \
-allocate between health and attack.')
-time.sleep(6)
-print('Each pokemon is of a different type. There are grass,fire and water kinds.\
-Grass is super effective on water, water is super effective on fire and fire is \
-super effective on grass! There are also pokemon with no type that are only super \
-effective against other pokemon with no type. Type super effectiveness \
-increases damage!')
-time.sleep(6)
 
-
-starterpokemonname=input("Enter your pokemon name: ") #Players can choose their pokemon's name
-ptype=0 #arbitrary value to enter the while loop
-types=['fire','water','grass'] #list of all the types to make sure entered type is valid
-while ptype not in types:
-    ptype=input("Enter your pokemon's type (fire,water,grass): ").lower()#Players choose their pokemon's type
-    if ptype not in types:
-        print('Not a valid type! Enter either fire, water or grass exactly.')#If an invalid type, ask for the type again
-
-print('You have waken up in a room with no apparent exit other than two doors. \
-You have no idea what has happened, \
-only that you were on your way to the prestigeous Pokemon Academy for \
-your first day of middle school with your very first pokemon. \
-Now, you appear to be in a locked room and only have that pokemon with you to help you escape. While \
-thinking about this situation, you can not help but wonder; why did this happen \
-to me? Where do these doors lead? How could I have ever ended up in this \
-situation...')
-
-
-effectivedict = {'water': 'fire',
-                 'fire': 'grass',
-                 'grass': 'water',
-                 'None': 'None'
-                 'psychic': 'fire', 'water', 'grass', 'None'}
-
-#Shows what type each type is effective against.
 class pokemon():
 
     def __init__(self, name, hp, attack, type):
@@ -120,10 +36,92 @@ class pokemon():
 
     def heal(self):
         self.hp = self.maxhp
+
+gameinfo1 = ['Welcome to the game!', 'The objective of this game is to defeat\
+the boss and escape the locked room.', 'You have to escape by battling pokemon.\
+If you beat all the guards, you can face the boss!', 'Each pokemon has health and attack stats. The attack stat is split up\
+between minimum and maximum attack stats. The minimum attack \
+stat is 0.7 times the maximum attack stat. When you level up, you get 5 stat points to \
+allocate between health and attack.', 'Each pokemon is of a different type. There are grass,fire and water kinds.\
+Grass is super effective on water, water is super effective on fire and fire is \
+super effective on grass! There are also pokemon with no type that are only super \
+effective against other pokemon with no type. Type super effectiveness \
+increases damage!']
+
+gameinfo2 =gameinfo2 = ['You have waken up in a room with no apparent exit other than two doors','You have no idea what \
+has happened, only that you were on your way to the prestigeous Pokemon Academy for \
+your first day of middle school with your very first pokemon.', 'Now, you appear to be in a locked room and only have that \
+pokemon with you to help you escape. While thinking about this situation, you can not help but wonder; why did this happen \
+to me? Where do these doors lead? How could I have ever ended up in this situation...']
+
+def room1():#Where you can level up your pokemon, then you go into the guard room.
+    print('Here you can level up your pokemon if you win a battle.')
+    wildencounter()
+    print ('You come across a huge hole and the only ways to get across are to walk over an old \
+    bridge or to walk around the hole')
+    print ('Do you take the bridge or walk around the hole?')
+    command = input().lower()
+    if 'bridge' in command:
+         print ('While you are taking the bridge, the bridge collapses, you fall down the hole and find a wild pokemon!!')
+         wildencounter()
+         print ('Luckily there is a ladder going up the hole, you take the ladder and reach the other side')
+         
+    else:
+         print ('You crossed the hole safely!')
+    pokecenter()
+    guardroom()        
         
+def room2():#Where you can level up your pokemon
+    print('Here you can level up your pokemon if you win a battle.')
+    wildencounter()
+    print ('Looks like this room is a dead end!')
+    print('You arrived back at the starting room!')
+    startingroom()
+       
+def startingroom(): #This is the first room players end up in
+    command=input('Both rooms look like they contain wild pokemon. Turn left or right?: ')
+    if 'left' in command:
+        room1()
+    elif 'right' in command:
+        room2()
+    else:
+        print('Please enter a valid direction!')
+        startingroom()
+        
+def pokecenter():
+    command = input('It looks like there is a Pokemon Center nearby, do you want to heal your pokemon?').lower()
+    if 'yes' in command:
+    	print ('Hello, and welcome to the Pokémon Center. We will heal your pokemon to full health.')
+    	starterpokemon.heal()
+    	print ('Please come again!')
+        
+def guardroom():
+    print('This is the guard room! You have to face 3 pokemon back to back!')
+    guardbattle()
+    pokecenter()
+    bossbattle()
+    
+def getstarterpokemon():
+    starterpokemonname=input("Enter your pokemon name: ") #Players can choose their pokemon's name
+    ptype=0 #arbitrary value to enter the while loop
+    types=['fire','water','grass'] #list of all the types to make sure entered type is valid
+    while ptype not in types:
+        ptype=input("Enter your pokemon's type (fire,water,grass): ").lower()#Players choose their pokemon's type
+        if ptype not in types:
+            print('Not a valid type! Enter either fire, water or grass exactly.')#If an invalid type, ask for the type again
+    starterpokemon=pokemon(starterpokemonname,50,10,ptype)
+
+#Shows what type each type is effective against.
+effectivedict = {'water': ['fire'],
+                 'fire': ['grass'],
+                 'grass': ['water'],
+                 'None': ['None'],
+                 'psychic': ['fire', 'water', 'grass', 'None']}
+    
 def is_effective(pokemon1, pokemon2):
-    if effectivedict[pokemon1.type] == pokemon2.type:
+    if pokemon2.type in effectivedict[pokemon1.type]:
         return True
+    return False
     
 def attack(pokemon1, pokemon2):
     attack = random.randint(pokemon1.minattack, pokemon1.maxattack)
@@ -149,11 +147,9 @@ def battle(pokemon1, pokemon2):
             break
         x = attack(pokemon2, pokemon1)
         if x==False:
-            print (‘{} fainted!’.format(starterpokemon.name))
+            print ('{} fainted!'.format(starterpokemon.name))
             print ( "YOU LOSE")
             sys.exit()
-
-starterpokemon=pokemon(starterpokemonname,50,10,ptype)
 
 wild1 = pokemon('Bidoof', random.randint(30, 40), 8,'None')
 wild2 = pokemon('Poliwag', random.randint(40, 45), 7, 'water')
@@ -167,6 +163,7 @@ guard2 = pokemon('Leafeon', 40, 11, 'grass')
 guard3 = pokemon('Flareon', 45, 11, 'fire')
 boss = pokemon('Meowth', 55, 14, 'psychic')
 
+wildlist = [wild1, wild2, wild3, wild4, wild5, wild6, wild7]
 guardlist = [guard1, guard2, guard3]
 dialoguelist = ['I am sure you must be wondering why you have been sent here. \
 Well I, Guardone cannot give you the answer. If you feel that you must escape, \
@@ -188,32 +185,38 @@ have to let you go for now, but know that there are much more dangerous groups o
 train but know that we are being targeted by some very dangerous organizations as well. Our deal is that we will help you train \
 your pokemon in exchange for helping protect us. Is that a deal?','THE END']
 
-wildlist = [wild1, wild2, wild3, wild4, wild5, wild6, wild7]
-
 def wildencounter():
     index = random.randint(0, 6)
     wild = wildlist[index]
     print ('A wild {} appeared!'.format(wild.name))
     battle(starterpokemon, wild)
 
-
 def guardbattle():
     for guard, dialogue in zip(guardlist, dialoguelist):
         print (dialogue)
         battle(starterpokemon, guard)
 
-
+def endingscene():
+    for dialogue in endingdialogue:
+        print(dialogue)
+        time.sleep(2.5)
+    sys.exit()        
+        
 def bossbattle():
     for dialogue in bossdialogue:
         print (dialogue)
         time.sleep(2.5)
     battle(starterpokemon, boss)
     endingscene()
-def endingscene():
-    for dialogue in endingdialogue:
-        print(dialogue)
-        time.sleep(2.5)
-    sys.exit()
     
-
-startingroom()
+def main():
+    for dialogue in gameinfo1:
+        print (dialogue)
+        time.sleep(5)
+    getstarterpokemon()
+    for dialogue in gameinfo2:
+        print (dialogue)
+        time.sleep(3)
+    startingroom()
+    
+main()
