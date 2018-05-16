6c58b5c442a14b4fe4c989e543147c1ed1029ae7
Zandew <39287272+Zandew@users.noreply.github.com>
2018-05-16 16:26:59 -0400

    Update sidequests.py

--- sidequests.py
+++ sidequests.py
@@ -1,12 +1,12 @@
-class sidequest():
+class sidequest(): #After creating instance of class, assign it to a pokemon 
   
   def __init__(self, name, reward):
     self.name = name
     self.initiated = False
-    self.reward = reward
+    self.reward = reward #Reward should be a function so when we complete the sidequest, we call the function
     
   def start_sidequest():
-    self.initiated = True
+    self.initiated = True #Whenever battle finishes (or any other event), checks if player has active sidequest
     
   def complete_sidequest():
     reward()
@@ -15,8 +15,8 @@ class NPC():
   
   def __init__(self, name):
     self.name = name
-    self.questlist = [
-    self.sidequest = self.questlist[random.randint()]
+    self.questlist = []
+    self.sidequest = self.questlist[random.randint(0, len(self.questlist)-1]
       
   def talk():
     print (dialogue)
