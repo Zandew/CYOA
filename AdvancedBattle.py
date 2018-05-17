31d791ce42cefe010907d67f7240233c7e417159
Zandew <39287272+Zandew@users.noreply.github.com>
2018-05-17 17:13:38 -0400

    Update AdvancedBattle.py

--- AdvancedBattle.py
+++ AdvancedBattle.py
@@ -1,1 +1,95 @@
+import random
+class pokemon():
 
+    def __init__(self, name, hp, attack, type):
+        self.name = name
+        self.hp = hp
+        self.maxattack = attack
+        self.maxhp = hp
+        self.type = type
+
+    @property
+    def minattack(self):
+        return round(self.maxattack*0.7)
+        
+    def attack(self, enemy):
+        attack = random.randint(self.minattack, self.maxattack)
+        enemy.hp = enemy.hp-attack
+        if enemy.hp<=0:
+            print ('{} has knocked out {}'.format(self.name, enemy.name))
+            return True
+        else:
+            print ('{} has attacked {} for {} damage'.format(self.name, enemy.name, attack))
+            return False
+        
+    def level_up(self):
+        print ('{} leveled up!'.format(self.name))
+        print ('You have gained 5 stat points! ')
+        health = int(input("How many stats do you want to allocate into HP?"))
+        while health<0 or health>5:
+            print('This is not a valid increase in health!')
+            health = int(input("How many stats do you want to allocate into HP?"))
+        print(5-health,'points were allocated into attack. ')
+        self.hp += health
+        self.maxattack += 5-health
+        
+    def heal(self):
+        self.hp = self.maxhp
+        
+class specialmove():
+    
+    def __init__(self, name, type, amount):
+        self.name = name
+        self.type = type
+        self.amount = amount
+        
+tackle = specialmove('tackle', 'damage', 7)
+heal = specialmove('heal', 'heal', 7)
+        
+class starterpokemon(pokemon):
+  
+  def __init__(self, name, maxhp, type):
+        self.name = name
+        self.maxhp = maxhp
+        self.type = type
+        self.moves = [tackle.name, heal.name]
+        
+  def attack(self, enemy):
+        print ('It is your turn!')
+        print ('What move do you want {} to use?!'.format(self.name))
+        print ('--------------------')
+        for move in self.moves:
+            print (move)
+        print ('--------------------')
+        while True:
+            move = input().lower()
+            if move not in self.moves:
+                continue
+            print ('{} used {}!'.format(self.name, move))
+        if move.type == 'damage':
+            attack = move1.amount
+            if is_effective(self, enemy):
+                attack += 2
+            enemy.hp = enemy.hp-attack
+            if enemy.hp<=0:
+                print ('{} has knocked out {}'.format(self.name, enemy.name))
+                return True
+            else:
+                print ('{} has attacked {} for {} damage'.format(self.name, enemy.name, attack))
+                return False 
+        if move.type == 'heal':
+            heal = move1.amount
+            print ('{} has healed for {}!'.format(self.name, heal))
+            self.hp += heal
+            if self.hp>self.maxhp:
+                self.hp = self.maxhp
+pikachu = starterpokemon('pikachu', '10', 'water')
+bulbasaur = pokemon('bulbasaur', '20', '2', 'grass')
+pikachu.attack('bulbasaur')
+            
+            
+        
+            
+    
+       
+    
