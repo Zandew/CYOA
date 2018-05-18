020214a0dd18ea866230a9be684f51829cb5f93b
raytonlin1 <39320843+raytonlin1@users.noreply.github.com>
2018-05-18 09:38:59 -0400

    Update ICSSummative.py

--- ICSSummative.py
+++ ICSSummative.py
@@ -115,12 +115,13 @@ class starterpokemon(pokemon):
     def heal(self):
         self.hp = self.maxhp
 
-gameinfo1 = ['Welcome to the game!', 'The objective of this game is to defeat \
+gameinfo1 = ['Welcome to the game!', 'The objective of this game is to defeat\
 the boss and escape the locked room.', 'You have to escape by battling pokemon.\
-If you beat all the guards, you can face the boss!', 'Each pokemon has health and attack stats. The attack stat is split up\
-between minimum and maximum attack stats. The minimum attack \
-stat is 0.7 times the maximum attack stat. When you level up, you get 5 stat points to \
-allocate between health and attack.', 'Each pokemon is of a different type. There are grass,fire and water kinds.\
+If you beat all the guards, you can face the boss!', 'When you beat a pokemon, \
+you level up gaining 3 health points. Other pokemon attack within a range, but \
+your pokemon is special where it can have different attacks that do different \
+damage and effects, but most of these attacks are only available through \
+sidequests.', 'Each pokemon is of a different type. There are grass,fire and water kinds.\
 Grass is super effective on water, water is super effective on fire and fire is \
 super effective on grass! There are also pokemon with no type that are only super \
 effective against other pokemon with no type. Type super effectiveness \
