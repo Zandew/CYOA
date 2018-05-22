67fee915f799f3cc5027c4374e8874db02e8c69a
Zandew <39287272+Zandew@users.noreply.github.com>
2018-05-22 09:37:34 -0400

    Update ICSSummative.py

--- ICSSummative.py
+++ ICSSummative.py
@@ -108,7 +108,7 @@ class starter_pokemon(pokemon):
                         heal = round((self.maxhp-self.hp)*heal)
                         print ('{} has healed for {}!'.format(self.name, heal))
                         self.hp += heal
-                        pp -= 1
+                        move.pp -= 1
                         break
                 else:
                     print ("You have no pp left for {}! Enter another move!".format(move))
