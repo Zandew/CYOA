d7c5df5b8c2f267c785e13e8a69eff0ca7c95b40
Zandew <39287272+Zandew@users.noreply.github.com>
2018-05-28 20:00:54 -0400

    Update help.py

--- help.py
+++ help.py
@@ -1,1 +1,10 @@
-def help
+def help():
+    print ("What type of help do you need?")
+    print ("1. Backstory")
+    print ("2. Objective")
+    print ("3. Pokemon")
+    print ("4. Sidequests")
+    command = input()
+    if command == '1':
+        for dialogue in gameinfo1:
+
